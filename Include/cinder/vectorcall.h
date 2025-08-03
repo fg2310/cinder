f4632e81279599c419781e5521cacf8d0ace5494
fg2310 <62403481+fg2310@users.noreply.github.com>
2025-08-04 00:27:43 +0200

    Delete Include directory

--- Include/cinder/vectorcall.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define Ci_Py_AWAITED_CALL_MARKER  ((size_t)1 << (8 * sizeof(size_t) - 2))
-#define Ci_Py_AWAITED_CALL(n) ((n)&Ci_Py_AWAITED_CALL_MARKER)
-#define Ci_Py_VECTORCALL_ARGUMENT_MASK    Ci_Py_AWAITED_CALL_MARKER
-
-/* Same as PyVectorcall_Call but allows passing extra flags to function being called */
-CiAPI_FUNC(PyObject *) Ci_PyVectorcall_Call_WithFlags(
-    PyObject *callable, PyObject *tuple, PyObject *kwargs, size_t flags);
